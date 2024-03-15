66dadeaeb5b144b3e032dcb8d7d902b5f2ad3e97
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-03-15 20:58:58 +0300

    Dashboard

---
+++ src/app/comp/layout/board-card/board-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-board-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './board-card.component.html',
+  styleUrl: './board-card.component.css'
+})
+export class BoardCardComponent {
+
+}
